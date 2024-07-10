6e170815dc9f92fee6ea99af960532055b9bf82c
Dimitri-F <dimitri.fonsat@gmail.com>
2024-07-11 01:16:46 +0200

    architecture

---
+++ db/connexion.php
@@ -0,0 +1,22 @@
+<?php
+// Charger les variables d'environnement depuis le fichier .env
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['SERVER'];
+$username = $_ENV['USER'];
+$password = $_ENV['PASSWORD'];
+
+
+try {
+    // On établit la connexion à la base de données
+    $conn = new PDO("mysql:host=$servername;dbname=bddtest", $username, $password);
+    // Définir le mode d'erreur PDO à exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connexion réussie';
+} catch (PDOException $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
+}
+
---
+++ src/Article.php
@@ -0,0 +1,42 @@
+<?php
+namespace App;
+
+class Article
+{
+    private $id;
+    private $title;
+    private $price;
+
+    public function __construct($id, $title, $price) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+}
